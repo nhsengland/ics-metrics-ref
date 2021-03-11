e154fed4456666de5192f8191e62dc8a64d81620
Ewan Wakeman <ewan.wakeman@nhs.net>
2021-03-11 14:00:39 +0000

    Adding writing templates

--- R/metadata.r
+++ R/metadata.r
@@ -6,14 +6,14 @@
 
 require('readr')
 
-get_indicators = function(return_type = "list") {
+get_indicators = function(return_type = "list", path = "./data/system_metrics_indicator_list.csv") {
     
     return_types <- c("list", "tibble")
     if(!(return_type %in% return_types)){
         stop("return_type must be one of \"list\" or \"tibble\"")
     }
 
-    inds <- read_csv("./data/system_metrics_indicator_list.csv", col_types = cols())
+    inds <- read_csv(path, col_types = cols())
 
     if(return_type == "list"){
         vec <- inds[["ind_code"]]
